eec722d5b1b36de43529f28d996988631bc2c3f0
Faith Janicek <faithe1937@gmail.com>
2019-08-28 04:15:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function dwarfRollCall(dwarves) {
-}
-
+ var dwarves = ['Doc', 'Dopey', 'Bashful', 'Grumpy']
+ 
+ function dwarfRollCall(dwarves){
+   var listDwarves = []
+   
+   for (var i=0;i<dwarves.length;i++){
+     listDwarves.push(`${i+1}. ${dwarves[i]} `)
+   }
+   return listDwarves.join("")
+ }
+ 
 function summonCaptainPlanet(planeteerCalls){
+ return planeteerCalls.map(a=>a.toUpperCase()+"!")
 }
 
 function longPlaneteerCalls(words) {
+  for(let i = 0; i < words.length; i++){
+   
+    if(words[i].length>4){
+      return true
+  }
+}
+  return false
 }
 
 function findTheCheese (foods) {
+  
+  for (var i = 0; i < foods.length; i++) {
+    
+    if (foods[i] ===  'gouda'|| foods[i]===  'cheddar' || foods[i]==='camembert') {
+      return foods[i];
+    }
+  }
+  return 'no cheese!'
 }
